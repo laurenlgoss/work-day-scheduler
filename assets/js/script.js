bbe173c40d3a2f8054ebc6a4db12e12bfd07a367
Lauren Goss <laurenlgoss98@gmail.com>
2021-07-13 17:39:38 -0400

    added event listener to save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 var $currentDay = $("#currentDay");
 var $descriptionArray = $(".description");
+var $saveButtonArray = $(".saveBtn");
+
+var $event09 = $("#event-09");
+var $event10 = $("#event-10");
+var $event11 = $("#event-11");
+var $event12 = $("#event-12");
+var $event13 = $("#event-13");
+var $event14 = $("#event-14");
+var $event15 = $("#event-15");
+var $event16 = $("#event-16");
+var $event17 = $("#event-17");
+var eventArray = [$event09, $event10, $event11, $event12, $event13, $event14, $event15, $event16, $event17];
 
 var currentTime = moment();
 
 function init() {
     // Display current day in header
     $currentDay.text(currentTime.format("dddd, MMMM Do"));
+
+    // Add event listener to all save buttons
+    for (var i = 0; i < $saveButtonArray.length; i++) {
+        $saveButtonArray[i].addEventListener("click", handleSave);
+    }
 
     colorTimeblocks();
 }
@@ -27,8 +44,8 @@ function colorTimeblocks() {
 }
 
 // Save events within timeblocks
-    // Create input field
-
+function handleSave($eventXX) {
+}
     // Create save button
         // Upon click,
 
