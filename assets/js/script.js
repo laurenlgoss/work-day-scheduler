a8d41260a0c23aa11d520c2c3df8909dc3e276b0
Lauren Goss <laurenlgoss98@gmail.com>
2021-07-13 13:21:33 -0400

    linked css/js, pseudocoded js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Current day displayed in header
+
+// Timeblocks color coded
+    // Past
+
+    // Present
+
+    // Future
+
+// Save events within timeblocks
+    // Create input field
+
+    // Create save button
+        // Upon click,
+
+        // Text saved in local storage
+
+    // When refreshed, events persist
